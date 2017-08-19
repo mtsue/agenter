374cf840077516f987665391cbc1feb25029f08e
mtsue <bolerock309@gmail.com>
2017-08-19 21:40:56 +0900

    init electron app

---
+++ main.js
@@ -0,0 +1,26 @@
+const url = require('url');
+const path = require('path');
+
+const electron = require('electron');
+const app = electron.app;
+
+const browserWindow = electron.BrowserWindow;
+
+function createWindow(){
+    let mainWindow = new browserWindow({
+        width: 500,
+        height: 500
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.resolve('index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
