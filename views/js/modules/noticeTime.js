8a5cd0d45cb34805c33889bdfbcfe138dfca007b
mtsue <bolerock309@gmail.com>
2017-08-20 20:12:34 +0900

    時報機能

---
+++ views/js/modules/noticeTime.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    setInterval(noticeTime, 1);
+}
+
+function noticeTime() {
+    let message = document.getElementsByClassName('message')[0];
+    let messageText = document.getElementsByClassName('text')[0];
+
+    let time = getTime();
+
+    if(time.minute === 10){
+        message.style.opacity = 1;
+        messageText.innerHTML = time.hour + '時 ですよー';
+    }else{
+        message.style.opacity = 0;
+    }
+
+}
+
+function getTime(){
+    let date = new Date();
+    
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+    return { hour, minute, second };
+}
