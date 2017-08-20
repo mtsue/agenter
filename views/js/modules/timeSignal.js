980016c2f90e90b5bda07ffbf972d3253ee5d8f7
mtsue <bolerock309@gmail.com>
2017-08-20 20:17:00 +0900

    rename noticeTime -> timeSignal

--- views/js/modules/timeSignal.js
+++ views/js/modules/timeSignal.js
@@ -2,19 +2,18 @@ window.onload = function() {
     setInterval(noticeTime, 1);
 }
 
-function noticeTime() {
+function timeSignal() {
     let message = document.getElementsByClassName('message')[0];
     let messageText = document.getElementsByClassName('text')[0];
 
     let time = getTime();
 
-    if(time.minute === 10){
+    if(time.minute === 10 && time.second <= 5){
         message.style.opacity = 1;
         messageText.innerHTML = time.hour + '時 ですよー';
     }else{
         message.style.opacity = 0;
     }
-
 }
 
 function getTime(){
